a122c2f3f13c15fb7366e8b773bd276429d6d505
Velika <pi@raspberrypi1.local>
2021-02-13 22:30:44 +0100

    collector.py - gpio nevek valtozoban, es mukodik

--- collector.py
+++ collector.py
@@ -22,9 +22,9 @@ GPIO.setmode(GPIO.BCM)
 ## - L2 mero melyik gpio-ra van csatlakoztatva
 ## - Bekerem az EON-os fogyasztasmeron szereplo erteket, es ahhoz kepest fogok szamolni
 
-#l1_gpio = int(input("Add meg, hogy az L1 fazis mero, melyik GPIO-ra van csatlakoztatva:"))
-#l2_gpio = int(input("Add meg, hogy az L2 fazis mero, melyik GPIO-ra van csatlakoztatva:"))
-#l3_gpio = int(input("Add meg, hogy az L3 fazis mero, melyik GPIO-ra van csatlakoztatva:"))
+l1_gpio = int(input("Add meg, hogy az L1 fazis mero, melyik GPIO-ra van csatlakoztatva:"))
+l2_gpio = int(input("Add meg, hogy az L2 fazis mero, melyik GPIO-ra van csatlakoztatva:"))
+l3_gpio = int(input("Add meg, hogy az L3 fazis mero, melyik GPIO-ra van csatlakoztatva:"))
 eon_mero = float(input("Add meg az EON meron szereplo aktualis fogyasztast:"))
 
 #Kapcsolodas az adatbazishoz:
@@ -40,38 +40,38 @@ print("Mysql connect success!")
 mycursor = db.cursor()
 
 #A harom GPIO beallitasa, hogy input vagy output-kent lesz hasznalva
-GPIO.setup(4, GPIO.IN)
-GPIO.setup(17, GPIO.IN)
-GPIO.setup(16, GPIO.IN)
+GPIO.setup(l1_gpio, GPIO.IN)
+GPIO.setup(l2_gpio, GPIO.IN)
+GPIO.setup(l3_gpio, GPIO.IN)
 print("Asd")
 
 try:
     while True:
-        if GPIO.input(4):
+        if GPIO.input(l1_gpio):
             print("L1", datetime.now())
             mycursor.execute("INSERT INTO TVOF11_1 (Datetime) VALUES (localtime)")
             db.commit()
             eon_mero = eon_mero + 0.0005
             print("EON:", eon_mero)
-            GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.setup(l1_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         sleep(0.1)
 
-        if GPIO.input(17):
+        if GPIO.input(l2_gpio):
             print("L2", datetime.now())
             mycursor.execute("INSERT INTO TVOF11_2 (Datetime) VALUES (localtime)")
             db.commit()
             eon_mero = eon_mero + 0.0005
             print("EON:", eon_mero)
-            GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.setup(l2_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         sleep(0.1)
 
-        if GPIO.input(16):
+        if GPIO.input(l3_gpio):
             print("L3", datetime.now())
             mycursor.execute("INSERT INTO TVOF11_3 (Datetime) VALUES (localtime)")
             db.commit()
             eon_mero = eon_mero + 0.0005
             print("EON:", eon_mero)
-            GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.setup(l3_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         sleep(0.1)
         
         
